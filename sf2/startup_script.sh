02aa8250f825cdc99059d9289223a4e9d31eb4e3
Raul Jimenez <officialrauljimenez@gmail.com>
2019-11-26 10:55:28 -0600

    fixed no audio issue

--- sf2/startup_script.sh
+++ sf2/startup_script.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #fluidsynth -i -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 /home/pi/Desktop/sf2/Test_-_test1.mid
-lxterminal -e fluidsynth -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 -g 0 & 
+lxterminal -e fluidsynth -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 -g 6 & 
 sleep 2; aconnect 20: 128:
 lxterminal -e python /home/pi/fluidSynthGUI/gui.py &
 if [ $? -eq 0 ]
