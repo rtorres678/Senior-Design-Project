c8d6c2dda00aae651fb3ca790044b6536073f3c1
Raul Jimenez <officialrauljimenez@gmail.com>
2019-11-26 10:58:34 -0600

    corrected to have updated python version

--- sf2/startup_script.sh
+++ sf2/startup_script.sh
@@ -2,7 +2,7 @@
 #fluidsynth -i -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 /home/pi/Desktop/sf2/Test_-_test1.mid
 lxterminal -e fluidsynth -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 -g 6 & 
 sleep 2; aconnect 20: 128:
-lxterminal -e python /home/pi/fluidSynthGUI/gui.py &
+lxterminal -e python3 /home/pi/fluidSynthGUI/gui.py &
 if [ $? -eq 0 ]
 then
     echo "Connection established."
