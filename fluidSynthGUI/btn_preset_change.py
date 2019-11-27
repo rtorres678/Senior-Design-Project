f3aec7d134f8774bb846a12b89d055a6c248c603
Raul Jimenez <officialrauljimenez@gmail.com>
2019-11-27 17:33:26 -0600

    added gain slider

--- fluidSynthGUI/btn_preset_change.py
+++ fluidSynthGUI/btn_preset_change.py
@@ -6,13 +6,14 @@ import socket
 # @param {int} offset change in soundfont file
 def btn_preset_change(label, soundfonts, current, offset):
 	new_sf2_index = (current[0] + offset) % len(soundfonts)
+	print(new_sf2_index)
 	label.config(text=soundfonts[new_sf2_index].split('/')[-1].split('_')[-1].split('.')[0].title().center(10))
 	current.pop()
 	current.append(new_sf2_index)
 
 	# send setting change to port localhost:9800
 	client_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client_connection.connect(('127.0.0.1', 9800))
+	client_connection.connect((socket.gethostname(), 9800))
 	command = 'load ' + soundfonts[new_sf2_index] + ' \n'
 	client_connection.send(command.encode())
 	client.close()
--- fluidSynthGUI/gui.py
+++ fluidSynthGUI/gui.py
@@ -6,8 +6,10 @@ from tkinter import Label
 from tkinter import Button
 from tkinter import PhotoImage
 from tkinter import ttk
+from tkinter import Scale
 
 from btn_preset_change import btn_preset_change
+from gain_command_change import gain_command_change
 
 cwd = os.getcwd()
 
@@ -17,7 +19,7 @@ option2 = os.path.join(cwd, 'sf2', 'JR_church.SF2')
 option3 = os.path.join(cwd, 'sf2', 'JR_male.sf2')
 option4 = os.path.join(cwd, 'sf2', 'JR_bells.SF2')
 option5 = os.path.join(cwd, 'sf2', 'JR_elepiano.sf2')
-option6 = os.path.join(cwd, 'sf2', 'JR_strings.sf2')
+option6 = os.path.join(cwd, 'sf2', 'JR_analog.sf2')
 soundfonts = [option0, option1, option2, option3, option4, option5, option6] 
 
 window = Tk()
@@ -28,14 +30,14 @@ window.title("Preset Manager")
 current = [0]
 
 #current track first row
-current_sound_static = Label(window, text='Current Sound', font=('monospace', 56))
-current_track = Label(window, text=soundfonts[current[0]].split('/')[-1].split('_')[-1].split('.')[0].title().center(10), font=("monospace", 56))
+current_sound_static = Label(window, text='Current Sound', font=('monospace', 26))
+current_track = Label(window, text=soundfonts[current[0]].split('/')[-1].split('_')[-1].split('.')[0].title().center(10), font=("monospace", 26))
 current_sound_static.grid(row=0, column=1)
 current_track.grid(row=1, column=1)
 
 #buttons on second row
-BUTTON_H = 512
-BUTTON_W = 512
+BUTTON_H = 200
+BUTTON_W = 200
 back_arrow_img = PhotoImage(file=os.path.join(cwd,'images' ,'back_arrow.png'))
 forward_arrow_img = PhotoImage(file=os.path.join(cwd,'images' ,'forward_arrow.png'))
 
@@ -52,9 +54,14 @@ back_arrow_btn = Button(window, image=back_arrow_img, height=BUTTON_H, width=BUT
 forward_arrow_btn = Button(window, image=forward_arrow_img, height=BUTTON_H, width=BUTTON_W, command= lambda: btn_preset_change(current_track, soundfonts, current, 1))
 
 #position buttons on grid
-back_arrow_btn.grid(row=2, column=0, padx=(25,150))
+back_arrow_btn.grid(row=2, column=0, padx=(10,50))
 forward_arrow_btn.grid(row=2, column=2)
 
+
+#make slider
+gain_slider = Scale(window, from_=10, to=0, width=35, length=200, command= lambda x: gain_command_change(gain_slider.get()))
+gain_slider.set(5)
+gain_slider.grid(row=2, column= 1)
 
 #starts
 window.mainloop()
