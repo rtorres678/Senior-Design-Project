b56a5ded5ba4e66d2da635b0c9e4d974eb759d6c
Roberto Torres <roberto.torres@mavs.uta.edu>
2019-12-03 13:58:06 -0600

    rename fluidsynthgui folder to pi_code

--- src/pi_code/startup_script.sh
+++ src/pi_code/startup_script.sh
@@ -2,10 +2,10 @@
 #fluidsynth -i -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 /home/pi/Desktop/sf2/Test_-_test1.mid
 lxterminal -e fluidsynth -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 -g 6 & 
 sleep 2; aconnect 20: 128:
-lxterminal -e python3 /home/pi/fluidSynthGUI/gui.py &
 if [ $? -eq 0 ]
 then
-    echo "Connection established."
+    echo "Connection established.\nStarting gui..."
+    lxterminal -e python3 /home/pi/fluidSynthGUI/gui.py &
     exit 0
 else
     echo "Failed to connect. Terminating Fluidsynth..."
