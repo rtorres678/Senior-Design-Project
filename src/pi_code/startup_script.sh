412ea7044fc8869b34985f2229ec6cabff43557b
Roberto Torres <roberto.torres@mavs.uta.edu>
2019-12-03 14:15:30 -0600

    create kill.sh modify startup_script

--- src/pi_code/startup_script.sh
+++ src/pi_code/startup_script.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 #fluidsynth -i -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 /home/pi/Desktop/sf2/Test_-_test1.mid
-lxterminal -e fluidsynth -s -a alsa /home/pi/Desktop/sf2/JR_sax.sf2 -g 6 & 
+echo "Starting Fluidsynth..."
+lxterminal -e fluidsynth -s -a alsa sf2/JR_sax.sf2 -g 6 & 
+echo "Done\nSearching for MIDI device..."
 sleep 2; aconnect 20: 128:
 if [ $? -eq 0 ]
 then
     echo "Connection established.\nStarting gui..."
-    lxterminal -e python3 /home/pi/fluidSynthGUI/gui.py &
+    lxterminal -e python3 gui.py &
     exit 0
 else
     echo "Failed to connect. Terminating Fluidsynth..."
