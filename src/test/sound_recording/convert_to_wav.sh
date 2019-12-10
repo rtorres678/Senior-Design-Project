7d4492678b57bceceea8a8be210b522e48d0b4db
Roberto Torres <roberto.torres@mavs.uta.edu>
2019-12-10 16:40:52 -0600

    fyuck you

--- src/test/sound_recording/convert_to_wav.sh
+++ src/test/sound_recording/convert_to_wav.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-fluidsynth -F out.wav ~/Senior-Design-Project/src/pi_code/sf2/JR_sax.sf2 ~/Senior-Design-Project/src/test/sound_recording/test.mid
+fluidsynth -g 10 -F out.wav ~/Senior-Design-Project/src/pi_code/sf2/JR_sax.sf2 ~/Senior-Design-Project/src/test/sound_recording/test2.mid
--- src/test/sound_recording/record.py
+++ src/test/sound_recording/record.py
@@ -3,18 +3,25 @@
 import os
 import serial
 import subprocess
-
+import signal
 port = "/dev/ttyACM0"
 
 ser = serial.Serial(port, 9600)
 ser.flushInput()
 
 count = 0
+PID = 0
 
-def func(bit):
-    p = subprocess.Popen('. ~/Senior-Design-Project/src/test/sound_recording/start.sh', shell=True)
-    if bit == 0:
-        p.kill();
+def func2(p):
+    p.kill()
+
+def func1():
+    #p = subprocess.Popen('. ~/Senior-Design-Project/src/test/sound_recording/start.sh', shell=True)
+    p = subprocess.Popen("exec " + '~/Senior-Design-Project/src/test/sound_recording/start.sh', stdout=subprocess.PIPE, shell=True)
+    print(p.pid)
+    return p
+    #if bit == 0:
+    #    p.kill();
 
 while True:
     #input = ser.read().decode("utf-8")
@@ -22,11 +29,10 @@ while True:
     if val == 'a':
         if count != 1:
             count = 1
-            #os.system("lxterminal -e . ./home/pi/Senior-Design-Project/src/test/sound_recording/start.sh -t killMe")
-            #subprocess.Popen('. ~/Senior-Design-Project/src/test/sound_recording/start.sh', shell=True)
-            func(1)
+            PID = func1()
+            #print(1)
     if val == 'z':
         if count != 0:
             count = 0
-            print('2')
-            func(0)
+            #print('2')
+            func2(PID)
