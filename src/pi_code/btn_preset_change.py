e03734b7a7f687127ca63254bde80ffff087b3c8
Raul Jimenez <officialrauljimenez@gmail.com>
2019-12-20 09:41:03 -0600

    cleaned up btn preset change and removed unnecessary socket module

--- src/pi_code/btn_preset_change.py
+++ src/pi_code/btn_preset_change.py
@@ -1,9 +1,3 @@
-import socket
-# @brief changes the preset based on offset, note current is a list so it can be modified
-# @param {Label} label tkinter label object
-# @param {list} soundfonts filepaths to soundfonts
-# @param {list} current current index of soundfonts list
-# @param {int} offset change in soundfont file
 def btn_preset_change(label, soundfonts, current, offset, client_connection):
 	new_sf2_index = (current[0] + offset) % len(soundfonts)
 	label.config(text=soundfonts[new_sf2_index].split('/')[-1].split('.')[0].replace('_', ' ').title().center(10))
